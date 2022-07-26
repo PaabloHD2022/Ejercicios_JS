fdfa124f007a6b6848fe3c9093a590701623e1b3
PaabloHD2022 <pablodemartini@gmail.com>
2022-07-26 19:07:56 -0300

    Agregado de funciones al trabajo

--- JS/main.js
+++ JS/main.js
@@ -4,110 +4,69 @@ EL EJERCICIO PREGUNTA NOMBRE COMPLETO - FECHA NAC - MES NAC - AÑO NAC
 (NO SE COMO CALCULAR LA EDAD PARA LOS QUE AL DIA DE LA FECHA NO CUMPLIERON AÑOS.)
 DEVUELVE NOMBRE COMPLETO - EDAD - SIGNO DEL ZODIACO.*/
 
-/*let nombreCompleto = prompt("Ingrese su nombre y apellido");
-let diaNac = parseInt(prompt("Ingrese dia de nacimento..."));
-let mesNac = parseInt(prompt("Ingrese numero de mes de nacimiento..."));
-let anioNac = parseInt(prompt("ingrese año de nacimento"));*/
 const anioActual = 2022;
-let diaNac = ""
-let mesNac = ""
-let anioNac = ""
 
 validacion()
-signoZodiaco(diaNac, mesNac)
 
 function validacion(){
-    let i = 0;
-    while(i<4){
+    let nombreCompleto = ""
+    let diaNac = ""
+    let mesNac = ""
+    let anioNac = ""
 
-        console.log(i)
 
-        do {
-            nombreCompleto = prompt("Ingrese su nombre y apellido");
-        }while(nombreCompleto!="")
-
-        i++
+    do {
+        nombreCompleto = prompt("Ingrese su nombre y apellido");
         diaNac = parseInt(prompt("Ingrese dia de nacimento..."));
         mesNac = parseInt(prompt("Ingrese mes de nacimento..."));
+        anioNac = parseInt(prompt("Ingrese anio de nacimento..."));   
+    }while(nombreCompleto.trim() == "" || isNaN(diaNac) || isNaN(mesNac) || isNaN(anioNac));
 
-        
+    const signo = signoZodiaco(diaNac, mesNac)
+    const edad = anioActual-anioNac;
 
-    }
+    alert(`Su nombre es ${nombreCompleto}, su edad es ${edad} y su signo zodiaco es ${signo}`);
 }
 
 function signoZodiaco(diaNac, mesNac){
 
-    switch (diaNac){
+    let signo = ""
 
-        case 
-            (((diaNac >=23) && (mesNac ==12)) || ((diaNac <=20) && (mesNac ==1))):
-            alert("Su signo es: Capricornio");
-            break;
+    if ((diaNac >=23 && mesNac ==12) || (diaNac <=20 && mesNac ==1))
+    signo = "Capricornio";
 
+    if ((diaNac >=21 && mesNac ==1) || (diaNac <= 19 && mesNac ==2))
+    signo = "Acuario";
 
-        case
-            ((diaNac >=21) && (mesNac ==1) || (diaNac <= 19) && (mesNac ==2)):
-            alert("Su signo es: Acuario");
-            break;
+    if ((diaNac >=20 && mesNac ==2) || (diaNac <=20 && mesNac ==3))
+    signo = "Piscis";
 
+    if ((diaNac >=21 && mesNac ==3) || (diaNac <=20 && mesNac ==4))
+    signo = "Aries";
 
-        case
-            ((diaNac >=20) && (mesNac ==2) || (diaNac <=20) && (mesNac ==3)):
-            alert("Su signo es: Piscis");
-            break;
+    if ((diaNac >=21 && mesNac ==4) || (diaNac <=21 && mesNac ==5))
+    signo = "Tauro";
 
-        case
-            ((diaNac >=21) && (mesNac ==3) || (diaNac <=20) && (mesNac ==4)):
-            alert("Su signo es: Aries");
-            break;
+    if ((diaNac >=22 && mesNac ==5) || (diaNac <=21 && mesNac ==6))
+    signo = "Geminis";
 
+    if ((diaNac >=22 && mesNac ==6) || (diaNac <= 23 && mesNac ==7))
+    signo = "Cancer";
 
-        case
-            ((diaNac >=21) && (mesNac ==4) || (diaNac <=21) && (mesNac ==5)):
-            alert("Su signo es: Tauro");
-            break;
+    if ((diaNac >=24 && mesNac ==7) || (diaNac <=23 && mesNac ==8))
+    signo = "Leo";
 
+    if ((diaNac >=24 && mesNac ==8) || (diaNac <=23 && mesNac ==9))
+    signo = "Virgo";
 
-        case
-            ((diaNac >=22) && (mesNac ==5) || (diaNac <=21) && (mesNac ==6)):
-            alert("Su signo es: Géminis");
-            break;
+    if ((diaNac >=24 && mesNac ==9) || (diaNac <=23 && mesNac ==10))
+    signo = "Libra";
 
+    if ((diaNac >=24 && mesNac ==10) || (diaNac <=22 && mesNac ==11))
+    signo = "Escorpio";
 
-        case    
-            ((diaNac >=22) && (mesNac ==6) || (diaNac <= 23) && (mesNac ==7)):
-            alert("Su signo es: Cancer");
-            break;
+    if ((diaNac >=23 && mesNac ==11) || (diaNac <=22 && mesNac ==12))
+    signo = "Sagitario";
 
-
-        case
-            ((diaNac >=24) && (mesNac ==7) || (diaNac <=23) && (mesNac ==8)):
-            alert("Su signo es: Leo");
-            break;
-
-
-        case
-            ((diaNac >=24) && (mesNac ==8) || (diaNac <=23) && (mesNac ==9)):
-            alert("Su signo es: Virgo");
-            break;
-
-
-        case
-            ((diaNac >=24) && (mesNac ==9) || (diaNac <=23) && (mesNac ==10)):
-            alert("Su signo es: Libra");
-            break;
-
-
-        case
-            ((diaNac >=24) && (mesNac ==10) || (diaNac <=22) && (mesNac ==11)):
-            alert("Su signo es: Escorpio");
-            break;
-
-
-        case
-            ((diaNac >=23) && (mesNac ==11) || (diaNac <=22) && (mesNac ==12)):
-            alert("Su signo es: Sagitario");
-            break;
-
-    }
+    return signo
 }
