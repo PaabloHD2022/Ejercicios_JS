7537fa5e69a60ab3de5e79ccfdea868d630ec4c6
PaabloHD2022 <pablodemartini@gmail.com>
2022-07-26 15:28:30 -0300

    Cambios en el JS

--- JS/main.js
+++ JS/main.js
@@ -34,49 +34,49 @@ while(!fechaUno && !fechaDos){
 }
 
 if((fechaUno >=23) && (fechaDos ==12) || (fechaUno <=20) && (fechaDos ==1)){
-    alert("Tu signo es: Capricornio");
+    alert("Su signo es: Capricornio");
 }
 
 else if((fechaUno >=21) && (fechaDos ==1) || (fechaUno <= 19) && (fechaDos ==2)){
-    alert("Tu signo es: Acuario");
+    alert("Su signo es: Acuario");
 }
 
 else if((fechaUno >=20) && (fechaDos ==2) || (fechaUno <=20) && (fechaDos ==3)){
-    alert("Tu signo es: Piscis");
+    alert("Su signo es: Piscis");
 }
 
 else if((fechaUno >=21) && (fechaDos ==3) || (fechaUno <=20) && (fechaDos ==4)){
-    alert("Tu signo es: Aries");
+    alert("Su signo es: Aries");
 }
 
 else if((fechaUno >=21) && (fechaDos ==4) || (fechaUno <=21) && (fechaDos ==5)){
-    alert("Tu signo es: Tauro");
+    alert("Su signo es: Tauro");
 }
 
 else if((fechaUno >=22) && (fechaDos ==5) || (fechaUno <=21) && (fechaDos ==6)){
-    alert("Tu signo es: Géminis");
+    alert("Su signo es: Géminis");
 }
 
 else if((fechaUno >=22) && (fechaDos ==6) || (fechaUno <= 23) && (fechaDos ==7)){
-    alert("Tu signo es: Cancer");
+    alert("Su signo es: Cancer");
 }
 
 else if((fechaUno >=24) && (fechaDos ==7) || (fechaUno <=23) && (fechaDos ==8)){
-    alert("Tu signo es: Leo");
+    alert("Su signo es: Leo");
 }
 
 else if((fechaUno >=24) && (fechaDos ==8) || (fechaUno <=23) && (fechaDos ==9)){
-    alert("Tu signo es: Virgo");
+    alert("Su signo es: Virgo");
 }
 
 else if((fechaUno >=24) && (fechaDos ==9) || (fechaUno <=23) && (fechaDos ==10)){
-    alert("Tu signo es: Libra");
+    alert("Su signo es: Libra");
 }
 
 else if((fechaUno >=24) && (fechaDos ==10) || (fechaUno <=22) && (fechaDos ==11)){
-    alert("Tu signo es: Escorpio");
+    alert("Su signo es: Escorpio");
 }
 
 else if((fechaUno >=23) && (fechaDos ==11) || (fechaUno <=22) && (fechaDos ==12)){
-    alert("Tu signo es: Sagitario");
+    alert("Su signo es: Sagitario");
 }
