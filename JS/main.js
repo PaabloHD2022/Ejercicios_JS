ecaaa6f96a3cc21aa00e00c04fc1653ea499e824
PaabloHD2022 <pablodemartini@gmail.com>
2022-07-26 18:25:33 -0300

    Arreglos

--- JS/main.js
+++ JS/main.js
@@ -4,79 +4,110 @@ EL EJERCICIO PREGUNTA NOMBRE COMPLETO - FECHA NAC - MES NAC - AÑO NAC
 (NO SE COMO CALCULAR LA EDAD PARA LOS QUE AL DIA DE LA FECHA NO CUMPLIERON AÑOS.)
 DEVUELVE NOMBRE COMPLETO - EDAD - SIGNO DEL ZODIACO.*/
 
-let nombreCompleto = prompt("Ingrese su nombre y apellido");
-let fechaUno = parseInt(prompt("Ingrese dia de nacimento..."));
-let fechaDos = parseInt(prompt("Ingrese numero de mes de nacimiento..."));
-let anio_Nac = parseInt(prompt("ingrese año de nacimento"));
-const anio_Actual = 2022;
+/*let nombreCompleto = prompt("Ingrese su nombre y apellido");
+let diaNac = parseInt(prompt("Ingrese dia de nacimento..."));
+let mesNac = parseInt(prompt("Ingrese numero de mes de nacimiento..."));
+let anioNac = parseInt(prompt("ingrese año de nacimento"));*/
+const anioActual = 2022;
+let diaNac = ""
+let mesNac = ""
+let anioNac = ""
 
+validacion()
+signoZodiaco(diaNac, mesNac)
 
-if(nombreCompleto ==""){
-    prompt("Ingrese su nombre y apellido");
+function validacion(){
+    let i = 0;
+    while(i<4){
+
+        console.log(i)
+
+        do {
+            nombreCompleto = prompt("Ingrese su nombre y apellido");
+        }while(nombreCompleto!="")
+
+        i++
+        diaNac = parseInt(prompt("Ingrese dia de nacimento..."));
+        mesNac = parseInt(prompt("Ingrese mes de nacimento..."));
+
+        
+
+    }
 }
 
-else {
-    alert("Nombre: " +nombreCompleto);
-}
+function signoZodiaco(diaNac, mesNac){
 
-if (isNaN(anio_Nac)) {
-    alert ("No ingresaste un numero en año de nacimiento");
-}
+    switch (diaNac){
 
-else{
-	let edad = anio_Actual - anio_Nac
-	alert ("Usted tiene: " + edad + " años de edad...");
-}
+        case 
+            (((diaNac >=23) && (mesNac ==12)) || ((diaNac <=20) && (mesNac ==1))):
+            alert("Su signo es: Capricornio");
+            break;
 
-while(!fechaUno && !fechaDos){
-    fechaUno = parseInt(prompt("Ingrese dia de nacimento..."));
-    fechaDos = parseInt(prompt("Ingrese mes de nacimento..."));
-}
 
-if((fechaUno >=23) && (fechaDos ==12) || (fechaUno <=20) && (fechaDos ==1)){
-    alert("Su signo es: Capricornio");
-}
+        case
+            ((diaNac >=21) && (mesNac ==1) || (diaNac <= 19) && (mesNac ==2)):
+            alert("Su signo es: Acuario");
+            break;
 
-else if((fechaUno >=21) && (fechaDos ==1) || (fechaUno <= 19) && (fechaDos ==2)){
-    alert("Su signo es: Acuario");
-}
 
-else if((fechaUno >=20) && (fechaDos ==2) || (fechaUno <=20) && (fechaDos ==3)){
-    alert("Su signo es: Piscis");
-}
+        case
+            ((diaNac >=20) && (mesNac ==2) || (diaNac <=20) && (mesNac ==3)):
+            alert("Su signo es: Piscis");
+            break;
 
-else if((fechaUno >=21) && (fechaDos ==3) || (fechaUno <=20) && (fechaDos ==4)){
-    alert("Su signo es: Aries");
-}
+        case
+            ((diaNac >=21) && (mesNac ==3) || (diaNac <=20) && (mesNac ==4)):
+            alert("Su signo es: Aries");
+            break;
 
-else if((fechaUno >=21) && (fechaDos ==4) || (fechaUno <=21) && (fechaDos ==5)){
-    alert("Su signo es: Tauro");
-}
 
-else if((fechaUno >=22) && (fechaDos ==5) || (fechaUno <=21) && (fechaDos ==6)){
-    alert("Su signo es: Géminis");
-}
+        case
+            ((diaNac >=21) && (mesNac ==4) || (diaNac <=21) && (mesNac ==5)):
+            alert("Su signo es: Tauro");
+            break;
 
-else if((fechaUno >=22) && (fechaDos ==6) || (fechaUno <= 23) && (fechaDos ==7)){
-    alert("Su signo es: Cancer");
-}
 
-else if((fechaUno >=24) && (fechaDos ==7) || (fechaUno <=23) && (fechaDos ==8)){
-    alert("Su signo es: Leo");
-}
+        case
+            ((diaNac >=22) && (mesNac ==5) || (diaNac <=21) && (mesNac ==6)):
+            alert("Su signo es: Géminis");
+            break;
 
-else if((fechaUno >=24) && (fechaDos ==8) || (fechaUno <=23) && (fechaDos ==9)){
-    alert("Su signo es: Virgo");
-}
 
-else if((fechaUno >=24) && (fechaDos ==9) || (fechaUno <=23) && (fechaDos ==10)){
-    alert("Su signo es: Libra");
-}
+        case    
+            ((diaNac >=22) && (mesNac ==6) || (diaNac <= 23) && (mesNac ==7)):
+            alert("Su signo es: Cancer");
+            break;
 
-else if((fechaUno >=24) && (fechaDos ==10) || (fechaUno <=22) && (fechaDos ==11)){
-    alert("Su signo es: Escorpio");
-}
 
-else if((fechaUno >=23) && (fechaDos ==11) || (fechaUno <=22) && (fechaDos ==12)){
-    alert("Su signo es: Sagitario");
+        case
+            ((diaNac >=24) && (mesNac ==7) || (diaNac <=23) && (mesNac ==8)):
+            alert("Su signo es: Leo");
+            break;
+
+
+        case
+            ((diaNac >=24) && (mesNac ==8) || (diaNac <=23) && (mesNac ==9)):
+            alert("Su signo es: Virgo");
+            break;
+
+
+        case
+            ((diaNac >=24) && (mesNac ==9) || (diaNac <=23) && (mesNac ==10)):
+            alert("Su signo es: Libra");
+            break;
+
+
+        case
+            ((diaNac >=24) && (mesNac ==10) || (diaNac <=22) && (mesNac ==11)):
+            alert("Su signo es: Escorpio");
+            break;
+
+
+        case
+            ((diaNac >=23) && (mesNac ==11) || (diaNac <=22) && (mesNac ==12)):
+            alert("Su signo es: Sagitario");
+            break;
+
+    }
 }
